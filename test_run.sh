f10d711c8cac3b2183d4913b0371ac521f2ee790
Henning <henning.holgersen@gmail.com>
2022-12-07 21:57:07 +0100

    added extra test stream

--- test_run.sh
+++ test_run.sh
@@ -3,3 +3,5 @@ echo "------------------ SAMPLE WITH KEY ------------------"
 cat stream1.jsonl | target-oracle --config config.json
 echo "------------------ SAMPLE WITHOUT KEY ------------------"
 cat stream2.jsonl | target-oracle --config config.json
+echo "------------------ The Horvath ------------------"
+cat stream3.jsonl | target-oracle --config config.json
